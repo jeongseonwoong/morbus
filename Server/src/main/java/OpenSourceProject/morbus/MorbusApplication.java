848de752e90c011979be4e0e5bb407aced880c8c
jeongseonwoong <tjsdnd1219@naver.com>
2024-04-09 13:25:27 +0900

    Create Server

---
+++ Server/src/main/java/OpenSourceProject/morbus/MorbusApplication.java
@@ -0,0 +1,13 @@
+package OpenSourceProject.morbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorbusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MorbusApplication.class, args);
+	}
+
+}
---
+++ Server/src/test/java/OpenSourceProject/morbus/MorbusApplicationTests.java
@@ -0,0 +1,13 @@
+package OpenSourceProject.morbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorbusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
