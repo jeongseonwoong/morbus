8254f86ebcb6bb82d7a74362fffadcaf308894c5
jeongseonwoong <tjsdnd1219@naver.com>
2024-04-23 22:03:15 +0900

    질병 정보변수 추가

--- Server/src/main/java/OpenSourceProject/VOclass/Disease.java
+++ Server/src/main/java/OpenSourceProject/VOclass/Disease.java
@@ -4,16 +4,28 @@ public class Disease {
 
         //info, prevent, solution
     private String name;
+    private String briefInfo;
+    private String description;
+
 
     public Disease(String inDis)
     {
         name=inDis;
     }
 
-    public String get()
+    public String getName()
     {
         return name;
     }
 
+    public String getBriefInfo()
+    {
+        return briefInfo;
+    }
+
+    public String getDescription()
+    {
+     return description;
+    }
 
 }
