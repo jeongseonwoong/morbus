404aec048497c3eae60feb36cfc61ce741ed7150
jeongseonwoong <tjsdnd1219@naver.com>
2024-04-28 14:57:19 +0900

    검색 내용이 올바르지 않을 경우 경고문구를 띄우도록 함

--- Server/src/main/resources/static/js/searchSymptomName.js
+++ Server/src/main/resources/static/js/searchSymptomName.js
@@ -1,8 +1,7 @@
- function searchVal() {
-    const searchText= document.querySelector("#search")
-     printf("fsdjfldf");
-     if(searchText.length===0||searchText.length>10)
-     {
-         alert("증상을 다시 입력해주세요");
-     }
+function searchVal() {
+    const searchText = document.querySelector("#search").value;
+    console.log("1234");
+    if (searchText.length === 0 || searchText.length > 10) {
+        alert("입력 길이가 올바르지 않습니다");
+    }
 }
