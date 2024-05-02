2febf62e780cdf56b4b7854a14a61194341dec3b
jeongseonwoong <tjsdnd1219@naver.com>
2024-05-02 19:41:30 +0900

    파일 읽지 못하는 오류해결

--- Server/src/main/java/OpenSourceProject/morbus/algorithm/SymptomSetting.java
+++ Server/src/main/java/OpenSourceProject/morbus/algorithm/SymptomSetting.java
@@ -1,10 +1,13 @@
 package OpenSourceProject.morbus.algorithm;
 import OpenSourceProject.VOclass.Disease;
 import OpenSourceProject.VOclass.Symptom;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.*;
@@ -14,8 +17,16 @@ public class SymptomSetting implements Setting {
 
     public Object JsonSetting() throws IOException, ParseException {
         JSONParser parser = new JSONParser();
-        Reader reader = new FileReader("src/main/resources/static/data/SymptomList.json");
-        return parser.parse(reader);
+        ClassPathResource resource = new ClassPathResource("static/data/SymptomList.json");
+        BufferedReader br = new BufferedReader(new InputStreamReader(resource.getInputStream()));
+        String s = "";
+        StringBuilder sb = new StringBuilder();
+        while((s = br.readLine()) != null){
+            sb.append(s);
+        }
+        ObjectMapper om = new ObjectMapper();
+        JsonNode jsonNode = om.readTree(sb.toString());
+        return parser.parse(sb.toString());
     }
 
     public ArrayList<String> toArr(JSONArray jsonArray) {
--- Server/src/test/java/OpenSourceProject/morbus/MorbusApplicationTests.java
+++ Server/src/test/java/OpenSourceProject/morbus/MorbusApplicationTests.java
@@ -1,13 +1,32 @@
 package OpenSourceProject.morbus;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
 
-@SpringBootTest
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 class MorbusApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void JsonSetting() throws IOException, ParseException {
+		JSONParser parser = new JSONParser();
+		ClassPathResource resource = new ClassPathResource("static/data/SymptomList.json");
+		BufferedReader br = new BufferedReader(new InputStreamReader(resource.getInputStream()));
+		String s = "";
+		StringBuilder sb = new StringBuilder();
+		while((s = br.readLine()) != null){
+			sb.append(s);
+		}
+		ObjectMapper om = new ObjectMapper();
+		JsonNode jsonNode = om.readTree(sb.toString());
+		System.out.println(jsonNode);
 	}
 
 }
