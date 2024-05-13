045a8c19a97ec13008188064d27051aba68ae3c4
YangHeeChang <84493979+YangHeeChang@users.noreply.github.com>
2024-05-13 15:44:03 +0900

    Revert "Database"

--- Server/src/main/java/OpenSourceProject/morbus/algorithm/SymptomRecordRepository.java
+++
@@ -1,6 +0,0 @@
-package OpenSourceProject.morbus.algorithm;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SymptomRecordRepository extends JpaRepository<SymptomRecord, Long> {
-}
