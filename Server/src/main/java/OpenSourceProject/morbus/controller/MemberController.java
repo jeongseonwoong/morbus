51d7ff8ccb88ffb81994609f45133f17e1219900
jeongseonwoong <tjsdnd1219@naver.com>
2024-05-15 14:34:47 +0900

    로그인 창 구현

--- Server/src/main/java/OpenSourceProject/morbus/controller/MemberController.java
+++ Server/src/main/java/OpenSourceProject/morbus/controller/MemberController.java
@@ -1,8 +1,13 @@
 package OpenSourceProject.morbus.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
-    
+
+    @GetMapping("/")
+    public String LogIn() {
+        return "login";
+    }
 }
