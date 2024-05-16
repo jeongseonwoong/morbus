85daaeeb4cec000a7295b67f00ac76ab8e2e6ed0
YangHeeChang <diogmlckd@naver.com>
2024-05-17 00:33:37 +0900

    SymptomRecordService 클래스 생성

---
+++ Server/src/main/java/OpenSourceProject/morbus/algorithm/SymptomRecordService.java
@@ -0,0 +1,9 @@
+package OpenSourceProject.morbus.algorithm;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class SymptomRecordService {
+}
