ac49d7e0898375a39219d1d3ccc75ce63e3b7d3f
YangHeeChang <diogmlckd@naver.com>
2024-05-17 01:25:24 +0900

    데이터베이스의 데이터를 불러오는 기능

--- Server/src/main/java/OpenSourceProject/morbus/algorithm/SymptomRecordService.java
+++ Server/src/main/java/OpenSourceProject/morbus/algorithm/SymptomRecordService.java
@@ -5,5 +5,12 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Service
 public class SymptomRecordService {
+    @Autowired
+    private SymptomRecordRepository symptomRecordRepository;
+
+    public List<SymptomRecord> getAllRecords() {
+        return symptomRecordRepository.findAll();
+    }
 }
