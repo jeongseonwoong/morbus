78e2333f4f5702ca40e38d09d24a625a64242fe3
YangHeeChang <diogmlckd@naver.com>
2024-05-17 00:17:23 +0900

    데이터베이스와 매핑할 엔티티 정의

--- Server/src/main/java/OpenSourceProject/morbus/algorithm/SymptomRecord.java
+++ Server/src/main/java/OpenSourceProject/morbus/algorithm/SymptomRecord.java
@@ -1,6 +1,13 @@
 package OpenSourceProject.morbus.algorithm;
 
 import jakarta.persistence.*;
+import java.time.LocalDateTime;
 
+@Entity
 public class SymptomRecord {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String symptom;
+    private LocalDateTime timestamp;
 }
