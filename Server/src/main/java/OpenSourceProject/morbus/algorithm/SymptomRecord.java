6acdb9a296616d2c349f2bca06bc69f38705f851
YangHeeChang <diogmlckd@naver.com>
2024-05-17 00:24:26 +0900

    id필드 값 반환 및 설정

--- Server/src/main/java/OpenSourceProject/morbus/algorithm/SymptomRecord.java
+++ Server/src/main/java/OpenSourceProject/morbus/algorithm/SymptomRecord.java
@@ -10,4 +10,11 @@ public class SymptomRecord {
     private Long id;
     private String symptom;
     private LocalDateTime timestamp;
+    public SymptomRecord() {}
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
