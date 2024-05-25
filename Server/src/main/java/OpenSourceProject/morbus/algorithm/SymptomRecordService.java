56c2230126a268c803668ce5d0d4a3918772b913
YangHeeChang <diogmlckd@naver.com>
2024-05-25 11:10:18 +0900

    id에 맞는 데이터 값 불러오기 및 저장

--- Server/src/main/java/OpenSourceProject/morbus/algorithm/SymptomRecordService.java
+++ Server/src/main/java/OpenSourceProject/morbus/algorithm/SymptomRecordService.java
@@ -1,5 +1,6 @@
 package OpenSourceProject.morbus.algorithm;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
@@ -10,14 +11,16 @@ public class SymptomRecordService {
     @Autowired
     private SymptomRecordRepository symptomRecordRepository;
 
-    public void saveSymptom(String symptom) {
+    public void saveSymptom(String symptom, HttpSession session) {
+        Long memberId = (Long) session.getAttribute("memberId");
         SymptomRecord record = new SymptomRecord();
         record.setSymptom(symptom);
         record.setTimestamp(LocalDateTime.now());
+        record.setRecordKey(memberId);
         symptomRecordRepository.save(record);
     }
 
-    public List<SymptomRecord> getAllRecords() {
-        return symptomRecordRepository.findAll();
+    public List<SymptomRecord> getAllRecords(Long memberId) {
+        return symptomRecordRepository.findByRecordKey(memberId);
     }
 }
