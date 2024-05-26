34d627b433afa718a86b888f7faafbf640f53235
jeongseonwoong <tjsdnd1219@naver.com>
2024-05-26 11:22:38 +0900

    관련된 질병 안나오는 증상들과 같은 질병 중복으로 나오는 증상  고침

---
+++ Server/src/main/resources/static/js/moreDiseaseList.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    var duplicateDisease = document.querySelectorAll("#duplicateDisease ul button");
+    duplicateDisease.value.forEach(disease=>{
+        console.log(disease);
+    })
+})
+
