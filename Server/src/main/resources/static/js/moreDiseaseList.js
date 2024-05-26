a70929ce77f5075cec115ccda804f828a64e36d6
jeongseonwoong <tjsdnd1219@naver.com>
2024-05-26 12:54:17 +0900

    질병 리스트 간단화 표시

--- Server/src/main/resources/static/js/moreDiseaseList.js
+++ Server/src/main/resources/static/js/moreDiseaseList.js
@@ -1,7 +1,42 @@
-document.addEventListener("DOMContentLoaded",()=>{
+document.addEventListener("DOMContentLoaded",(event)=>{
     var duplicateDisease = document.querySelectorAll("#duplicateDisease ul button");
-    duplicateDisease.value.forEach(disease=>{
-        console.log(disease);
-    })
+    var diseaseList=document.querySelectorAll("#square ul button");
+    console.log(diseaseList.length);
+    console.log(duplicateDisease.length);
+    if(duplicateDisease.length>5 || diseaseList.length>5)
+    {
+        var hidingList = hideList();
+        var button = makeButton();
+        button.addEventListener("click",(event)=>{
+            event.preventDefault();
+            if(hidingList.style.display==="none")
+            {
+                button.innerText = "증상에 따른 질병 리스트 접기";
+                hidingList.style.display="block";
+            }
+            else
+            {
+                button.innerText = "증상에 따른 질병 리스트 더보기";
+                hidingList.style.display="none"
+            }
+        })
+    }
 })
+
+function hideList(event)
+{
+    var hidingList = document.querySelector("#square");
+    hidingList.style.display='none';
+    return hidingList;
+}
+
+function makeButton()
+{
+    var showMoreListBttn = document.createElement("button");
+    showMoreListBttn.innerText = "증상에 따른 질병 리스트 더보기";
+    showMoreListBttn.className="content";
+    var insertPosition = document.querySelector("#square");
+    document.querySelector("form").insertBefore(showMoreListBttn,insertPosition);
+    return showMoreListBttn;
+}
 
