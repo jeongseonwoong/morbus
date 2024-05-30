953850b62f9385f49c2df34cdd43a53f8f5698f8
user <gurugikr23@naver.com>
2024-05-30 19:36:45 +0900

    로그아웃 기능 id(단일) -> name(다중)으로 변경

--- Server/src/main/resources/static/js/Logout.js
+++ Server/src/main/resources/static/js/Logout.js
@@ -1,22 +1,41 @@
 document.addEventListener("DOMContentLoaded",()=>{
-    document.getElementById("logout").addEventListener("click",()=>{
+    var logout = document.getElementsByName("logout");
+    logout[0].addEventListener("click",()=> {
         console.log("a")
         var xhr = new XMLHttpRequest();
-        xhr.open("POST","logout",true);
-        xhr.setRequestHeader("Content-Type","application/json");
+        xhr.open("POST", "logout", true);
+        xhr.setRequestHeader("Content-Type", "application/json");
 
         xhr.send();
         console.log("b")
 
-        xhr.onreadystatechange = function() {
+        xhr.onreadystatechange = function () {
             if (xhr.readyState === 4 && xhr.status === 200) {
                 responseJson = JSON.parse(xhr.responseText);// JSON 응답 파싱
                 console.log(responseJson);
-                var responseUrl =responseJson.url; // 최종 URL
+                var responseUrl = responseJson.url; // 최종 URL
                 console.log(responseUrl);
                 window.location.replace(responseUrl);
             }
         };
+        logout[1].addEventListener("click",()=> {
+            console.log("a")
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", "logout", true);
+            xhr.setRequestHeader("Content-Type", "application/json");
 
+            xhr.send();
+            console.log("b")
+
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    responseJson = JSON.parse(xhr.responseText);// JSON 응답 파싱
+                    console.log(responseJson);
+                    var responseUrl = responseJson.url; // 최종 URL
+                    console.log(responseUrl);
+                    window.location.replace(responseUrl);
+                }
+            };
+        });
     });
 });
